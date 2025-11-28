ce86e3fffb0b729e2a147f3c383a4e9321c6eec1
santola.developer@gmail.com <santola.developer@gmail.com>
2025-11-28 12:32:48 +0700

    build themeToggle

---
+++ vue-cli/src/components/ThemeToggle.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="p-3">
+    <button @click="toggle" class="btn btn-success rounded-pill">
+      Switch to {{ theme === "light" ? "dark" : "light" }} mode
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["theme"],
+  methods: {
+    toggle() {
+      this.$emit("toggleTheme");
+    },
+  },
+};
+</script>
