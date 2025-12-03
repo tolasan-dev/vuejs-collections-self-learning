904025b951d69e968e1fa4c7eaf0509f5e43208d
santola.developer@gmail.com <santola.developer@gmail.com>
2025-12-03 15:27:20 +0700

    update project

---
+++ vue-js-vite/vue-project/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <h1>Hello Vue</h1>
+
+  <div>
+    <p>Number: {{ num }}</p>
+    <p>Message: {{ message }}</p>
+
+    <button @click="counting" type="button" class="btn btn-primary">
+      Count
+    </button>
+    <button @click="outstring" type="button" class="btn btn-danger">
+      Show Message
+    </button>
+    <button @click="changeObject" type="button" class="btn btn-success">
+      Show
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+let object = { name: "san tola" };
+
+let num = ref(10);
+let message = ref("");
+
+function counting() {
+  num.value++;
+}
+
+function outstring() {
+  message.value = "Hello vue";
+}
+
+function changeObject() {
+  // Object.name = "Mr B";
+  console.log(Object.value.name);
+}
+</script>
+
+<style>
+body {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+}
+</style>
--- vue-js-vite/vue-vite/src/App.vue
+++ vue-js-vite/vue-vite/src/App.vue
@@ -1,47 +1,20 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <p>Number : {{ num }}</p>
+  <br />
+  <p>String: {{ string }}</p>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script>
+// import { ref } from "vue"; 
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+export default {
+  setup() {
+    let num = ref(10);
+    let string = ref("Hello vue js with version running by vite");
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+    return { num, string };
+  },
+};
+</script>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style></style>
