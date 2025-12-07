da0fb2a4b7e2ffd438d0412355720b1d4aca9e1f
santola.developer@gmail.com <santola.developer@gmail.com>
2025-12-07 15:49:51 +0700

    upgrade ui component fontend

---
+++ vue-js-composition-api-router/section-route/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Navbar />
+    <div class="container mt-4">
+      <router-view />
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
