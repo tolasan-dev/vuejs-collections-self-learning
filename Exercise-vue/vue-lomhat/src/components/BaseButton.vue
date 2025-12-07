0be721b5e09d3cef2a2f8efcb400a472a09c7f4b
santola.developer@gmail.com <santola.developer@gmail.com>
2025-12-07 15:57:25 +0700

    upgrade ui component fontend

---
+++ Exercise-vue/vue-lomhat/src/components/BaseButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="base-btn">
+    <slot />
+  </button>
+</template>
+
+<script setup></script>
+
+<style>
+.base-btn {
+  padding: 8px 12px;
+  border-radius: 6px;
+  background: #42b983;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+.base-btn:hover {
+  background-color: #2e8f63;
+}
+</style>
