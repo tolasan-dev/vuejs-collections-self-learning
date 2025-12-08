e7173014aedda135a4b65c4ecaded487dfb48211
santola.developer@gmail.com <santola.developer@gmail.com>
2025-12-08 16:09:37 +0700

    add emit and pros implements

---
+++ pros_emit/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h3>Dark Mode: {{ toggle }}</h3>
+
+  <ThemeChild @toggle="ToggleSwitch" />
+</template>
+
+<script setup>
+import { ref } from "vue";
+import ThemeChild from "./components/ThemeChild.vue";
+
+const toggle = ref(false);
+
+function ToggleSwitch(value) {
+  toggle.value = value;
+
+}
+</script>
---
+++ pros_emit/src/components/ThemeChild.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="handleClick">Toggle Dark Mode</button>
+  <h3>Child Switch: {{ localValue }}</h3>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const emit = defineEmits(["toggle"]);
+
+const localValue = ref(false);
+
+function handleClick() {
+  
+  localValue.value = !localValue.value;
+  emit("toggle", localValue.value);
+}
+</script>
