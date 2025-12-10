d6658376247e1bbd649ebe5eeea48debf6b65ffc
santola.developer@gmail.com <santola.developer@gmail.com>
2025-12-10 11:32:33 +0700

    dyanamic component

--- pros_emit/src/App.vue
+++ pros_emit/src/App.vue
@@ -1,17 +1,27 @@
 <template>
-  <h3>Dark Mode: {{ toggle }}</h3>
+  <div class="container mt-5 mx-auto">
+    <h2 class="text-center mb-4">Render Dynamic Widgets</h2>
 
-  <ThemeChild @toggle="ToggleSwitch" />
-</template>
+    <div class="d-flex gap-3 mb-4">
+      <button class="btn btn-success w-100" @click="isUserCard= true">User</button>
+      <button class="btn btn-primary w-100"  @click="isUserCard = false">Stats</button>
+    </div>
+
+    <!-- <UserCard />
+    <StatsCard /> -->
+    <!-- Dynamic Component with KeepAlive -->
+    <keep-alive>
+      <component :is="currentComponent" />
+    </keep-alive>
+
+  </div>
+</template>  
 
 <script setup>
-import { ref } from "vue";
-import ThemeChild from "./components/ThemeChild.vue";
+  import {computed, ref } from 'vue';
+  import StatsCard from './components/StatsCard.vue'
+  import UserCard from './components/UserCard.vue'
 
-const toggle = ref(false);
-
-function ToggleSwitch(value) {
-  toggle.value = value;
-
-}
+  const isUserCard = ref(true);
+  const currentComponent = computed(() => (isUserCard.value ? UserCard : StatsCard));
 </script>
---
+++ pros_emit/src/components/UserCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card border-success">
+    <div class="card-body">
+      <h3 class="card-title text-success">User Card</h3>
+
+      <input type="text" class="form-control mb-3" v-model="name" placeholder="Enter name" />
+      <p class="fw-bold">Name: {{ name }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+  const name = ref('')
+</script>
