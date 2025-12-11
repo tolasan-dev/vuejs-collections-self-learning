19b7bc5d338f07140c2f2b3471ddc062a4f3f791
santola.developer@gmail.com <santola.developer@gmail.com>
2025-12-11 14:29:34 +0700

    create router components

---
+++ vue-js-composition-api-router/vue-project/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav>
+    <!-- <router-link to="/">Home</router-link> -->
+    <!-- <router-link to="/about">About</router-link> -->
+  </nav>
+
+  <router-view />
+
+  <h1>Hello router</h1>
+</template>
