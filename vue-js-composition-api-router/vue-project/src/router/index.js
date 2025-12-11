a64416a57c27e81603a658e1b854e2609ece3129
santola.developer@gmail.com <santola.developer@gmail.com>
2025-12-11 16:53:54 +0700

    update code

--- vue-js-composition-api-router/vue-project/src/router/index.js
+++ vue-js-composition-api-router/vue-project/src/router/index.js
@@ -2,10 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import homepage from "@/components/Home/homepage.vue";
 import Contact from "@/components/Home/Contact.vue";
 import About from "@/components/Home/About.vue";
-
-// Product pages
-import ProductList from "@/components/Proudct/ProductList.vue";
-import ProductDetail from "@/components/Proudct/ProductDetail.vue";
+import ProductList from "@/components/Product/ProductList.vue";
+import ProductDetail from "@/components/Product/ProductDetail.vue";
 
 const routes = [
   { path: "/", name: "Home", component: homepage },
@@ -16,7 +14,7 @@ const routes = [
     component: About,
   },
   { path: "/products", component: ProductList },
-  { path: "/products/:id", component: ProductDetail },
+  { path: "/products/:id/:categoryId?", component: ProductDetail },
 ];
 
 const router = createRouter({
